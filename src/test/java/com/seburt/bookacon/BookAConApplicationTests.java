ba94309f861e314cf873812ab4b94b8509985380
seburt <seburt1980@gmail.com>
2023-02-01 17:06:10 +0200

    tests init

--- src/test/java/com/seburt/bookacon/BookAConApplicationTests.java
+++ src/test/java/com/seburt/bookacon/BookAConApplicationTests.java
@@ -1,13 +1,31 @@
 package com.seburt.bookacon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class BookAConApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        this.mockMvc.perform(
+                        get("/conference")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
     }
 
 }
