4fd43100e8bf90fb4408f94caa03f06da405d554
seburt <seburt1980@gmail.com>
2023-02-01 13:09:06 +0200

    init commit

---
+++ src/main/java/com/seburt/bookacon/BookAConApplication.java
@@ -0,0 +1,13 @@
+package com.seburt.bookacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAConApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seburt/bookacon/BookAConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seburt.bookacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAConApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
